a35c2bf14b6e49c52c2963458afee1dddd8f0800
Samuel <samuel.buenofran@gmail.com>
2023-03-03 22:05:09 -0300

    Finished main code for activity 4

---
+++ act4/index.js
@@ -0,0 +1,15 @@
+import PromptSync from "prompt-sync";
+import chalk from "chalk";
+
+const print = console.log;
+const read = new PromptSync();
+
+let temp = 0;
+for (let i = 1; i < 6; i++) {
+    temp += Number(read(chalk.green(`Enter input #${i}: `)));
+}
+
+print(temp / 5);
+
+ 
+
