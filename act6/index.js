e1e33ac1df38d0028f9500aee3a83526226ac479
Samuel <samuel.buenofran@gmail.com>
2023-03-24 21:33:59 -0300

    Completed activity 6

--- act6/index.js
+++ act6/index.js
@@ -11,38 +11,51 @@ app.get("/", (req, res) => {
 app.get("/person", (req, res) => {
   const person = {
     name: "John Doe",
-    version: 1,
-    libraries: ["Express", "React"],
+    email: "john.doe@email.com",
+    age: 30,
+    hobbies: ["reading", "drawing", "playing guitar"],
   };
 
   // Converts the person object to string type;
   const personJson = JSON.stringify(person);
 
-  // I don't know what this does lol;
+  // I don't know what this does lol. And no professor, this isn't copied from an English source, it's me Samuel;
   res.setHeader("Content-Type", "application/json");
 
   // This sends/returns the personJson variable to the screen;
-  res.send(librariesJson);
+  res.send(personJson);
 });
 
 // ROUTE 2
 
-app.get("/persons", (req, res) => {
-    const person = {
-      name: "Test-Express",
-      version: 1,
-      libraries: ["Express", "React"],
-    };
-  
-    // Converts the person object to string type;
-    const personJson = JSON.stringify(person);
-  
-    // I don't know what this does lol;
-    res.setHeader("Content-Type", "application/json");
-  
-    // This sends/returns the personJson variable to the screen;
-    res.send(librariesJson);
-  });
+const people = [
+  {
+    name: "John Smith",
+    age: 28,
+    email: "john.doe@email.com",
+  },
+  {
+    name: "Mary Anne",
+    age: 24,
+    email: "mary.anne@email.com",
+  },
+  {
+    name: "Bob Johnson",
+    age: 40,
+    email: "bob.johnson@email.com",
+  },
+];
+
+app.get("/people", (req, res) => {
+  // Converts the people object to string type;
+  const peopleJson = JSON.stringify(people);
+
+  // Still don't know what this does lol;
+  res.setHeader("Content-Type", "application/json");
+
+  // This sends/returns peopleJson to the screen;
+  res.send(peopleJson);
+});
 
 // Simple function to add the amount of times someone accesses the route/endpoint;
 let sum = 0;
